b2e007f927bfb6eb4211d82a8fa2ef682c242df6
Fabian Seluska <fabianseluska@gmail.com>
2021-03-19 16:55:44 +0100

    Added HTTPS support.

---
+++ config/important.md
@@ -0,0 +1,2 @@
+# Important info about the .key and .crt
+**You want to use your own generated keyfile and certificate; these are only for testing.**
--- index.js
+++ index.js
@@ -1,17 +1,25 @@
+import https from 'https'
 import express from 'express'
 import api from './api/api.js'
 import uptime from './lib/uptime.js'
 import settings from './lib/settings.js'
+import fs from 'fs'
 
+const credentials = {
+    key: fs.readFileSync("./config/private.key"),
+    cert: fs.readFileSync("./config/certificate.crt")
+}
 
 const server = express()
+const serverHttps = https.createServer(credentials, server)
+
 
 server.use(express.json())
 server.use(express.urlencoded({ extended: true }))
 
 server.use('/', api)
 
-server.listen(settings.get("port"), () => {
+serverHttps.listen(settings.get("port"), () => {
     uptime.setStartTime()
 
     console.log("Server started!")
