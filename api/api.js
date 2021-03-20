d93e08dd54cfb4ab35c3dcdb8152682808ad6a50
Fabian Seluska <fabianseluska@gmail.com>
2021-03-21 00:22:25 +0100

    Added API for devices/clients.
    The API allows you to connect, delete, trust, and get info about devices (clients).

--- api/api.js
+++ api/api.js
@@ -3,12 +3,13 @@ import { Router } from 'express'
 import serverApi from './server.js'
 import castApi from './cast.js'
 import settingsApi from './settings.js'
+import deviceApi from './device.js'
 
 const api = Router()
 
 api.use('/server', serverApi)
 api.use('/cast', castApi)
 api.use('/settings', settingsApi)
-
+api.use('/device', deviceApi)
 
 export default api
---
+++ api/device.js
@@ -0,0 +1,46 @@
+import { Router } from 'express'
+import crypto from 'crypto'
+import devices from '../lib/devices.js'
+import auth from '../lib/auth.js'
+
+const deviceApi = Router()
+
+deviceApi.post('/connect', (req, res) => {
+    const device = {
+        deviceName: req.header('deviceName') ? req.header('deviceName') : "Unnamed Device",
+        deviceID: crypto.randomBytes(16).toString('hex'),
+        key: crypto.randomBytes(32).toString('base64'),
+        trusted: false
+    }
+
+    devices.add(device)
+
+    res.json(device)
+})
+
+deviceApi.delete('/remove/:deviceID', auth.checkForLocalhost, (req, res) => {
+    devices.remove(req.params.deviceID)
+    res.end()
+})
+
+deviceApi.get('/pendingRequests', auth.checkForLocalhost, (req, res) => {
+    res.json(
+        devices.getPending()
+    )
+})
+
+deviceApi.put('/confirmRequest/:deviceID', auth.checkForLocalhost, (req, res) => {
+    devices.trust(req.params.deviceID)
+    res.end()
+})
+
+deviceApi.get('/info/:deviceID', auth.checkForLocalhost, (req, res) => {
+    const deviceInfo = devices.getInfo(req.params.deviceID)
+    res.json(deviceInfo)
+})
+
+deviceApi.get('/test', (req, res) => 
+    res.send(req.header("deviceKey") == devices.getInfo(req.header("deviceID")).key)
+)
+
+export default deviceApi
