1ec97f82d370cb3656567fddc3cea71a8fa801f1
Fabian Seluska <fabianseluska@gmail.com>
2021-03-25 21:47:01 +0100

    Basic implementation of the casting thingmadoo.
    The basics of dynamically loading different handlers to use with the casting. This will make it possible to (at least in theory) support plugins.

--- api/cast.js
+++ api/cast.js
@@ -1,5 +1,24 @@
 import { Router } from 'express'
+import auth from '../lib/auth.js'
+import casting from '../lib/casting.js'
+
 
 const castApi = Router()
+
+const castTypes = casting.castTypes
+
+
+castApi.post('/:type' /*, auth.checkClient*/, (req, res) => {
+    let sendMessage = "That media type can't be casted to this server."
+
+    castTypes.forEach(castType => {
+        if (castType.type == req.params.type) {
+            castType.handler()
+            sendMessage = "Casted!"
+        }
+    })
+
+    res.send(sendMessage)
+})
 
 export default castApi
