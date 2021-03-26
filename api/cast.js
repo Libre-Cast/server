8af0c2a8f4ebfe0359c4baebd005ebe14cfd59ff
Fabian Seluska <fabianseluska@gmail.com>
2021-03-26 13:48:26 +0100

    Casting is kind of fixed.
    Casting now kind of works I guess. Although, as of now, you can only cast images via URLs. You can use base64 encoded URLs or "normal" URLs.

--- api/cast.js
+++ api/cast.js
@@ -4,21 +4,42 @@ import casting from '../lib/casting.js'
 
 
 const castApi = Router()
-
 const castTypes = casting.castTypes
 
 
-castApi.post('/:type' /*, auth.checkClient*/, (req, res) => {
+castApi.post('/:type', auth.checkClient, (req, res) => {
     let sendMessage = "That media type can't be casted to this server."
 
     castTypes.forEach(castType => {
         if (castType.type == req.params.type) {
-            castType.handler()
+            castType.handler(req)
             sendMessage = "Casted!"
         }
     })
 
     res.send(sendMessage)
+})
+
+castApi.get('/types', auth.checkClient, (req, res) => {
+    let sendback = []
+
+    castTypes.forEach(castType =>
+        sendback.push(castType.type)
+    )
+
+    return res.json(sendback)
+})
+
+castApi.get('/show', auth.checkForLocalhost, (req, res) => {
+    let sendBack = "There's no media on the server!"
+
+    castTypes.forEach(castType => {
+        if (castType.type == casting.getMedia().type) {
+            sendBack = castType.builder()
+        }
+    })
+
+    res.send(sendBack)
 })
 
 export default castApi
--- api/server.js
+++ api/server.js
@@ -3,6 +3,8 @@ import uptime from '../lib/uptime.js'
 import settings from '../lib/settings.js'
 import makeQrCode from '../lib/qrcode.js'
 import notify from '../lib/notify.js'
+import casting from '../lib/casting.js'
+
 
 
 const serverApi = Router()
@@ -11,22 +13,14 @@ serverApi.get('/status', (_, res) => res.json({
     name: settings.get("name"),
     uptime: uptime.getElapsedTime(),
     allowConnections: settings.get("allowConnections"),
-    casting: {
-        type: "Image",
-        media: {
-            name: "Image.jpg"
-        }
-    }
+    casting: casting.getMedia()
 }))
 
 serverApi.get('/status/allowConnections', (_, res) => res.json(settings.get("allowConnections")))
 
-serverApi.get('/status/casting', (_, res) => res.json({
-    type: "Image",
-    media: {
-        name: "Image.jpg"
-    }
-}))
+serverApi.get('/status/casting', (_, res) => 
+    res.json(casting.getMedia())
+)
 
 serverApi.get('/qrCode/:type', (req, res) => res.send(makeQrCode(req.params.type)))
 
