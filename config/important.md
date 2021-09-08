fe9c1457b54a833f5c2946949787c69417b8aa25
Fabian Seluska <fabianseluska@gmail.com>
2021-09-08 08:40:15 +0200

    Removed HTTPS.

--- config/important.md
+++
@@ -1,2 +0,0 @@
-# Important info about the .key and .crt
-**You want to use your own generated keyfile and certificate; these are only for testing.**
--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-import https from 'https'
 import express from 'express'
 import api from './api/api.js'
 import uptime from './lib/uptime.js'
 import settings from './lib/settings.js'
-import fs from 'fs'
-
-const credentials = {
-    key: fs.readFileSync("./config/private.key"),
-    cert: fs.readFileSync("./config/certificate.crt")
-}
 
 const server = express()
-const serverHttps = https.createServer(credentials, server)
 
 
 server.use(express.json())
@@ -19,7 +11,7 @@ server.use(express.urlencoded({ extended: true }))
 
 server.use('/', api)
 
-serverHttps.listen(settings.get("port"), () => {
+server.listen(settings.get("port"), () => {
     uptime.setStartTime()
 
     console.log("Server started!")
